88f14f0a38f685acae3bf09dae026a8e47715705
cccswann <cccswann@gmail.com>
2021-03-19 18:49:35 -0700

    Create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+path = Path.create(title: 'Genki I', description: 'Review lessons from this textbook')
+
+lesson = Lesson.create(title: 'Lesson 1', path: path)
+
+grammar_point = GrammarPoint.create(title: 'です', description: 'to be/is', lesson: lesson)
+
+video = Video.create([
+    {title: 'What Exactly Does "Desu" Mean? (Japanese 101)', description:'We see it in anime, we see it in Japan. It\'s a Japanese word shrouded in mystery. But... is it even a word?', url: '\'https://www.youtube.com/embed/zEGji0v8JUQ\'', grammar_point: grammar_point},
+    {title: 'Da/desu sentence structure. How it really works. Easy keys to Japanese', description:'Desu is deceptively simple. But once you know what it really means, it is honestly simple!', url: '\'https://www.youtube.com/embed/iV5rjLeFIXo\'', grammar_point: grammar_point},
+    {title: '#1 は & です Japanese Grammar Lesson for Absolute Beginners', description:'X は Y です = X is Y * は is not "to be"!!!', url: '\'https://www.youtube.com/embed/UneYOL0DQxk\'', grammar_point: grammar_point}
+])
